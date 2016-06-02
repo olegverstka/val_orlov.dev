75dac6900dc235383e20a13486b506b6ae51c85f
Oleg Balyuk <olegverstka@yandex.ru>
2016-06-02 13:25:11 +0300

    Сверстан блок галереи

--- slicing/app/js/main.js
+++ slicing/app/js/main.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
-	$(".bxslider").bxSlider({
-		minSlides : 5,
-		maxSlides : 5,
-		moveSlides: 1,
-		slideWidth : 386
-	});
 
-	$(".bxreviews").bxSlider({
-		minSlides : 3,
-		maxSlides : 3,
-		moveSlides: 1,
-		slideWidth : 690
+	$('.slider_photo').owlCarousel({
+		center: true,
+		items:2,
+		loop:true,
+		margin:88,
+		//autoplay:true,
+    	//autoplayTimeout:3000,
+    	nav:true,
+		responsive:{
+			600:{
+				items:4
+			}
+		}
 	});
+	// $(".bxslider").bxSlider({
+	//  minSlides : 5,
+	//  maxSlides : 5,
+	//  moveSlides: 1,
+	//  slideWidth : 386
+	// });
+
+	// $(".bxreviews").bxSlider({
+	//  minSlides : 3,
+	//  maxSlides : 3,
+	//  moveSlides: 1,
+	//  slideWidth : 690
+	// });
 });
