00ea8c3f57214d916ce6ead46daabd4597fa39d4
Oleg Balyuk <olegverstka@yandex.ru>
2016-06-02 16:59:20 +0300

    Сверстал контент полностью, остался футтер

--- slicing/app/js/main.js
+++ slicing/app/js/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
 
+	$('.publication_slider').owlCarousel({
+		center: true,
+		items:3,
+		loop:true,
+		margin:189,
+		//autoplay:true,
+    	//autoplayTimeout:3000,
+    	nav:true,
+		responsive:{
+			600:{
+				items:1
+			}
+		}
+	});
+	$('.slider_review').owlCarousel({
+		center: true,
+		items:3,
+		loop:true,
+		margin:189,
+		//autoplay:true,
+    	//autoplayTimeout:3000,
+    	nav:true,
+		responsive:{
+			600:{
+				items:1
+			}
+		}
+	});
 	$('.slider_photo').owlCarousel({
 		center: true,
 		items:2,
